1349c6d3659d76638d02ad1f1b735d38baa325e7
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-31 11:33:05 -0500

    Finish Knight and implement to_s.

--- knight.rb
+++ knight.rb
@@ -7,20 +7,37 @@ class Knight
   attr_reader :location, :moves
 
   def initialize(location_x, location_y)
-    @location = location.new(location_x, location_y)
-    @moves = generate_moves
+    @location = Location.new(location_x, location_y)
+    @moves = []
+    update_possible_moves
   end
 
-  def generate_moves
-    move_array = []
-    move_array.push(location.new(@location.x - 1, @location.y - 2))
-    move_array.push(location.new(@location.x - 2, @location.y - 1))
-    move_array.push(location.new(@location.x - 2, @location.y + 1))
-    move_array.push(location.new(@location.x - 1, @location.y + 2))
-    move_array.push(location.new(@location.x + 1, @location.y + 2))
-    move_array.push(location.new(@location.x + 2, @location.y + 1))
-    move_array.push(location.new(@location.x + 2, @location.y - 1))
-    move_array.push(location.new(@location.x + 1, @location.y - 2))
-    move_array
+  def to_s
+    output = "Current Location: #{@location}\n" \
+             "Available moves: \n#{@moves[0].to_s}"
+    output += @moves[1..].reduce('') { |string, loc| string + ", " + loc.to_s} if @moves.size >= 1
+    output
+  end
+
+  private
+
+  def update_possible_moves
+    add_move(Location.new(@location.x - 1, @location.y - 2))
+    add_move(Location.new(@location.x - 2, @location.y - 1))
+    add_move(Location.new(@location.x - 2, @location.y + 1))
+    add_move(Location.new(@location.x - 1, @location.y + 2))
+    add_move(Location.new(@location.x + 1, @location.y + 2))
+    add_move(Location.new(@location.x + 2, @location.y + 1))
+    add_move(Location.new(@location.x + 2, @location.y - 1))
+    add_move(Location.new(@location.x + 1, @location.y - 2))
+    nil
+  end
+
+  def add_move(new_loc)
+    @moves.push(new_loc) if new_loc.valid?
+    nil
   end
 end
+
+k1 = Knight.new(1, 1)
+puts k1
--- location.rb
+++ location.rb
@@ -8,4 +8,14 @@ class Location
     @x = x_loc
     @y = y_loc
   end
+
+  def valid?
+    return true if @x >= 1 && @x <= 8 && y >= 1 && y <= 8
+
+    false
+  end
+
+  def to_s
+    [@x, @y].to_s
+  end
 end
