05a793224fbd382a80b4cc8c9e959c70cb4df872
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-01 13:12:57 -0500

    Remove all methods relating to pieces.

--- board.rb
+++ board.rb
@@ -5,31 +5,10 @@ require_relative './knight'
 
 # Chessboard class
 class Board
-  attr_accessor :x_size, :y_size, :piece
+  attr_accessor :x_size, :y_size
 
   def initialize(x_size, y_size)
     @x_size = x_size
     @y_size = y_size
   end
-
-  def place_knight(x_loc, y_loc)
-    @piece = Knight.new(x_loc, y_loc)
-    @piece.generate_paths(@x_size, @y_size)
-  end
-
-  def knight_moves(start, target)
-    place_knight(start[0], start[1])
-    @piece.bfs(target)
-  end
 end
-
-x = Board.new(4, 4)
-x.place_knight(1, 1)
-x.piece.pretty_print
-puts x.knight_moves([1, 1], [4, 4])
-
-p = Space.new(1, 1)
-q = Space.new(2, 2)
-p.predecessor = q
-
-puts p.predecessor_list
