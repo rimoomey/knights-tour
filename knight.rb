72a3dedc7eef8a7d1eeb4c0bb52f4c52d12c4262
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-02 08:50:20 -0500

    Iron out knight implementation.

--- knight.rb
+++ knight.rb
@@ -4,24 +4,30 @@ require_relative './piece'
 
 # Polymorphic Knight class
 class Knight < Piece
-  attr_reader :location, :moves, :neighbors
+  attr_reader :moves
 
   def initialize(coordinates)
     super(coordinates)
-    @moves = moveset
-    @neighbors = []
+    @legal_moves = moveset
   end
 
   def moveset
+    result = []
     moves = [
       [-2, -1], [-2, 1], [-1, -2], [-1, 2],
       [2, -1], [2, 1], [1, -2], [1, 2]
     ]
 
-    moves.map do |shift|
-      coordinates + shift
+    moves.each do |shift|
+      move = [coordinates[0] + shift[0], coordinates[1] + shift[1]]
+      next unless move[0].between?(1, 8) && move[1].between?(1, 8)
+
+      result << move
     end
+    result
   end
 end
 
 k = Knight.new([1, 1])
+p k.coordinates
+p k.moveset
--- piece.rb
+++ piece.rb
@@ -1,19 +1,11 @@
 # frozen_string_literal: true
 
-require_relative './space'
-
 # Class to represent knight on a chess board.
 class Piece
-  attr_reader :location
+  attr_reader :coordinates, :neighbors
 
   def initialize(coordinates)
-    @location = Space.new(coordinates[0], coordinates[1])
-    @location.visited = true
-  end
-
-  def move(new_location)
-    new_location.predecessor = @location
-    @location = new_location
-    @location.visited = true
+    @coordinates = coordinates
+    @neighbors = []
   end
 end
