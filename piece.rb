a00d92c415aa87bac73378878ac7338343343427
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-02 08:30:29 -0500

    Delete board.rb for incorrectness.

--- piece.rb
+++ piece.rb
@@ -4,7 +4,7 @@ require_relative './space'
 
 # Class to represent knight on a chess board.
 class Piece
-  attr_reader :space, :historical_moves
+  attr_reader :space
 
   def initialize(x_coord, y_coord)
     @space = Space.new(x_coord, y_coord)
